86e6ff5df85305eb940b43f1f4d4f7ab6ae7a2a1
omarzer0 <omarzero1015@gmail.com>
2021-08-21 07:19:11 +0200

    Init project and add missing dependencies

--- app/src/main/java/az/zero/movietime/ui/MainActivity.kt
+++ app/src/main/java/az/zero/movietime/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package az.zero.movietime
+package az.zero.movietime.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import az.zero.movietime.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
