dfeeeb12640d37f52c78ed7e7e8427757cb8bc4c
omarzer0 <omarzero1015@gmail.com>
2021-08-29 10:43:53 +0200

    change drawer icons

--- app/src/main/java/az/zero/movietime/repository/ShowRepository.kt
+++ app/src/main/java/az/zero/movietime/repository/ShowRepository.kt
@@ -2,7 +2,6 @@ package az.zero.movietime.repository
 
 import androidx.paging.Pager
 import androidx.paging.PagingConfig
-import androidx.paging.liveData
 import az.zero.movietime.api.MoviePagingSource
 import az.zero.movietime.api.ShowApi
 import az.zero.movietime.utils.MethodToCall
@@ -23,45 +22,4 @@ class ShowRepository @Inject constructor(
         config = PagingConfig(pageSize = 20, maxSize = 100, enablePlaceholders = false),
         pagingSourceFactory = { MoviePagingSource(showApi, methodToCall, showType, showId,searchQuery) }
     ).flow
-
-
-//    fun getShows2(
-//        methodToCall: MethodToCall = MethodToCall.GET_POPULAR,
-//        showType: ShowType = ShowType.MOVIE,
-//        showId: Int = -1,
-//        searchQuery: String = ""
-//    ) = Pager(
-//        config = PagingConfig(pageSize = 20, maxSize = 100, enablePlaceholders = false),
-//        pagingSourceFactory = { MoviePagingSource(showApi, methodToCall, showType, showId,searchQuery) }
-//    ).flow
-//
-//    fun getShows3(
-//        methodToCall: MethodToCall = MethodToCall.GET_POPULAR,
-//        showType: ShowType = ShowType.MOVIE,
-//        showId: Int = -1,
-//        searchQuery: String = ""
-//    ) = Pager(
-//        config = PagingConfig(pageSize = 20, maxSize = 100, enablePlaceholders = false),
-//        pagingSourceFactory = { MoviePagingSource(showApi, methodToCall, showType, showId,searchQuery) }
-//    ).flow
-//
-//    fun getShows4(
-//        methodToCall: MethodToCall = MethodToCall.GET_POPULAR,
-//        showType: ShowType = ShowType.MOVIE,
-//        showId: Int = -1,
-//        searchQuery: String = ""
-//    ) = Pager(
-//        config = PagingConfig(pageSize = 20, maxSize = 100, enablePlaceholders = false),
-//        pagingSourceFactory = { MoviePagingSource(showApi, methodToCall, showType, showId,searchQuery) }
-//    ).flow
-//
-//    fun getShows5(
-//        methodToCall: MethodToCall = MethodToCall.GET_POPULAR,
-//        showType: ShowType = ShowType.MOVIE,
-//        showId: Int = -1,
-//        searchQuery: String = ""
-//    ) = Pager(
-//        config = PagingConfig(pageSize = 20, maxSize = 100, enablePlaceholders = false),
-//        pagingSourceFactory = { MoviePagingSource(showApi, methodToCall, showType, showId,searchQuery) }
-//    ).flow
 }
